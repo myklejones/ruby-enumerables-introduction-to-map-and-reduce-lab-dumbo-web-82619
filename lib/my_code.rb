96b42767522aadeca9bbc6dae40db462f47a9a1b
Mykle jones <mykle.j91@gmail.com>
2019-08-12 20:40:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,11 @@ return total
 end
 
 def reduce_to_all_true(source_array)
-  
+  i=0 
+  while i<source_array.length
+  return true if source_array[i]
+  i+=1 
+end
 end
  
  def reduce_to_any_true(source_array)
